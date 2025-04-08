fcc71ff8aa317f9d43308bd64353b3a074563a56
Артём Упоров <aruporoff@yandex.ru>
2025-04-08 09:54:00 +0500

    Установлены пакеты, подключение к базе данных, переименованы новигационные свойства

---
+++ Models/AnimeTitle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppTitlesAnime.Models;
+
+public partial class AnimeTitle
+{
+    private Type type = null!;
+
+    public int Id { get; set; }
+
+    public short IdType { get; set; }
+
+    public string OriginalName { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public short Duration { get; set; }
+
+    public short CountSeries { get; set; }
+
+    public string? Description { get; set; }
+
+    public string Studio { get; set; } = null!;
+
+    public string? Poster { get; set; }
+
+    //навигационные свойства
+    public virtual ICollection<AnimeTitlesGenre> AnimeTitlesGenres { get; set; } = new List<AnimeTitlesGenre>();
+
+    public virtual Type Type { get => type; set => type = value; }
+    public virtual ICollection<TitlesStatus> TitlesStatuses { get; set; } = new List<TitlesStatus>();
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppTitlesAnime.Models;
+
+public partial class Genre
+{
+    public short Id { get; set; }
+
+    public string GenresName { get; set; } = null!;
+
+    //навигационные свойства
+    public virtual ICollection<AnimeTitlesGenre> AnimeTitlesGenres { get; set; } = new List<AnimeTitlesGenre>();
+}
